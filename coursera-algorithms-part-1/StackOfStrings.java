221746634e4b5e412a6e68c9fcaf580c47f22d32
namseok.kim <namseok.kim@lge.com>
2017-08-09 08:07:12 +0900

    LinkedList

---
+++ coursera-algorithms-part-1/StackOfStrings.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class StackOfStrings {
+    public static void main(String[] args) {
+        LinkedStackOfStrings stack = new LinkedStackOfStrings();
+
+        try {
+        Scanner scan = new Scanner(System.in);
+       // int i = scan.nextInt();
+        String s = scan.nextLine();
+
+        System.out.println(s);
+        } catch(Exception e) {
+        }
+
+        /*
+        while(!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if (s.equals("-")) StdOut.print(stack.pop());
+            else stack.push(s);
+        }
+        */
+    }
+}
